5a393ef719eeeb2afb33dc824c591a71f43e0307
Danil Aziz <danilaziz548@gmail.com>
2026-04-17 16:34:02 +0800

    fix github pages blank deploy

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "app",
+  publicDir: "public",
   plugins: [react()],
-  base: "/",
+  base: "/docs/",
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true,
+    assetsDir: "assets",
+  },
   server: {
     host: "127.0.0.1",
   },
