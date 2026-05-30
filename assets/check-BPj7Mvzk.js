43437926b667e62ca5da7c4e6baeb7343d96ac50
Danil Aziz <danilaziz548@gmail.com>
2026-05-30 12:49:23 +0800

    Publish latest build to main root

--- assets/check-BPj7Mvzk.js
+++ assets/check-BPj7Mvzk.js
@@ -1,4 +1,4 @@
-import{c}from"./createLucideIcon-cRy1iApG.js";/**
+import{c}from"./index-DgaberuL.js";/**
  * @license lucide-react v0.539.0 - ISC
  *
  * This source code is licensed under the ISC license.
